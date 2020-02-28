a0d9f4cd76d56fd1a8cbcef8d7c63140c469b48a
Alex Little <alex@digital-campus.org>
2020-02-28 10:09:23 +0100

    update

--- learning_rebellion/urls.py
+++ learning_rebellion/urls.py
@@ -7,5 +7,6 @@ from learning_rebellion import views as lr_views
 urlpatterns = [
     url(r'^$', lr_views.home_view, name="lr_home"),
     url(r'^news/', include('blog.urls')),
-    url(r'^(?P<podcast_slug>\w[\w/-]*)$', lr_views.podcast_view, name="podcast_article"),
+    url(r'^podcast/$', lr_views.podcast_home_view, name="podcast_home"),
+    url(r'^podcast/(?P<podcast_slug>\w[\w/-]*)$', lr_views.podcast_view, name="podcast_article"),
 ]
--- learning_rebellion/views.py
+++ learning_rebellion/views.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 from django.shortcuts import render
 from django.utils.translation import ugettext_lazy as _
 from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
 
 from learning_rebellion.models import Podcast, Tracker, Page
 from blog.models import  Blog
@@ -25,7 +26,26 @@ def home_view(request):
                            'podcasts': podcasts,
                            'page': get_page('home'),
                            'news': news })
+  
+def podcast_home_view(request):
+    site_tracker.send(sender=None, request=request)
+    podcasts = Podcast.objects.filter(active=True).order_by('-date_display')
+    paginator = Paginator(podcasts, 5)
     
+    try:
+        page = int(request.GET. get('page', '1'))
+    except ValueError:
+        page = 1
+        
+    try:
+        podcasts = paginator.page(page)
+    except (EmptyPage, InvalidPage):
+        podcasts = paginator.page(paginator.num_pages)
+    
+    return render(request, 'podcast/home.html',
+                          {'page': podcasts,
+                           'podcast_active': True})
+      
 def podcast_view(request, podcast_slug):
     site_tracker.send(sender=None, request=request)
     preview = request.GET.get("preview", 0)
