7f28076760ebc9249d1e287686a52e6663007aeb
Alex Little <alex@digital-campus.org>
2020-03-07 18:05:28 +0100

    add projects

--- projects/models.py
+++ projects/models.py
@@ -9,20 +9,23 @@ class Project(models.Model):
     title = models.TextField(blank=False)
     slug = AutoSlugField(populate_from='title', max_length=100, blank=True, null=True)
     body = RichTextField()
-    image = models.FileField(upload_to="images", blank=True, default=None)
+    image = models.FileField(upload_to="projects/images", blank=True, default=None)
     active = models.BooleanField(default=False)
     order = models.IntegerField(default=0, null=False)
     
     def __str__(self):
         return self.title
     
+    def get_links(self):
+        return ProjectLink.objects.filter(project=self, active=True).order_by('order')
+    
 class ProjectLink(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     create_date = models.DateTimeField(default=timezone.now)
     title = models.TextField(blank=False)
     slug = AutoSlugField(populate_from='title', max_length=100, blank=True, null=True)
-    description = RichTextField()
-    image = models.FileField(upload_to="images", blank=True, default=None)
+    description = RichTextField(blank=True, default=None)
+    image = models.FileField(upload_to="projects/links/images", blank=True, default=None)
     active = models.BooleanField(default=False)
     order = models.IntegerField(default=0, null=False)
     url = models.TextField(blank=False)
