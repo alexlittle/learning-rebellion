98533643ef627b65378c44b67b7723a5f6b6fcab
Alex Little <alex@digital-campus.org>
2020-06-06 18:31:26 +0300

    add projects

--- learning_rebellion/admin.py
+++ learning_rebellion/admin.py
@@ -7,19 +7,20 @@ from learning_rebellion.models import Podcast, Tracker, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title',  'menu_title', 'slug', 'active')
     search_fields = ['title',  'menu_title', 'slug', 'content']
- 
+
+
 class PodcastAdmin(admin.ModelAdmin):
     list_display = ('title', 'show_preview_url', 'date_display', 'active')  
     search_fields = ['title',  'body', 'slug']
        
-    def show_preview_url(self,obj):
+    def show_preview_url(self, obj):
         return format_html("<a href="+reverse('podcast_article', args={obj.slug}) + "?preview=1>"+ obj.title +" - preview</a>")
     
     show_preview_url.short_description = "Preview"
-    
+
+
 class TrackerAdmin(admin.ModelAdmin):
     list_display = ('tracker_date', 'ip', 'url', 'agent') 
-    
 
 
 admin.site.register(Page, PageAdmin)      
